3628c5a9487808ab4cba8b684eb15071a600103c
evilbutcool73 <bboyjosh73@gmail.com>
2024-02-19 16:20:30 +0100

    liaison avec le back

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
-<template>
-  <div class="bg-white grid grid-flow-rows grid-cols-2 gap-4 sm:grid-cols-4" >
+<template >
+  <div class="bg-white grid grid-flow-rows grid-cols-2 gap-4 sm:grid-cols-4 p-2">
     <div 
     class="mx-auto px-4 border-l-4 border-black box-content text-black text-nowrap" 
-    v-for="(material, index) in materials" :key="index">
+    v-for="(material, id) in materials" :key="id">
       <img src="C:\Users\jojoe\Documents\USMB\Project\S4\logo\RentaVan\bleu\logo.png" alt="test">
-      <div class="underline decoration-violet-500 hover:no-underline hover:text-violet-500 cursor-pointer"> <!-- Added text-purple-500 class -->
+      <div class="underline decoration-violet-500 hover:no-underline hover:text-violet-500 cursor-pointer"> 
         {{ material.name }}
       </div>
       <div>          
@@ -15,18 +15,25 @@
 </template>
 
 <script>
+
+import axios from 'axios';
+
 export default {
   data() {
     return {
-      materials: [
-        { name: 'Vélo Enfant', description: 'Description 1', price: '15', state : 'good' },
-        { name: 'Material 2', description: 'Description 2', price: '20', state: 'bad' },
-        { name: 'Material 3', description: 'Description 3', price: '20', state: 'bad' },
-        { name: 'Material 4', description: 'Description 4', price: '20', state: 'bad' },
-        { name: 'Material 5', description: 'Description 5', price: '20', state: 'bad' },
-        // Add more materials as needed
-      ],
+      materials: []
     };
   },
+  mounted() {
+    axios.get("http://localhost:3008/api/materials")
+      .then(response => {
+        // Assuming your API returns an array of materials
+        this.materials = response.data;
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching materials:', error);
+      });
+  }
 };
 </script>
