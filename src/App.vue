90df69cf2cf211ed697a01ebb9438937338e4f09
evilbutcool73 <bboyjosh73@gmail.com>
2024-03-10 14:36:04 +0100

    ajout de la description + confing tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,78 @@
-<template >
-  <div class="bg-white grid grid-flow-rows grid-cols-2 gap-4 sm:grid-cols-4 p-2">
-    <div 
-    class="mx-auto px-4 border-l-4 border-black box-content text-black text-nowrap" 
-    v-for="(material, id) in materials" :key="id">
-      <img src="C:\Users\jojoe\Documents\USMB\Project\S4\logo\RentaVan\bleu\logo.png" alt="test">
-      <div class="underline decoration-violet-500 hover:no-underline hover:text-violet-500 cursor-pointer"> 
-        {{ material.name }}
+<template>
+  <div class="bg-tertiary grid grid-flow-rows grid-cols-2 gap-4 sm:grid-cols-4 ">
+    <div
+      :class="{ 'col-span-full': clickedElement === id }"
+      class="flex flex-row"
+      v-for="(material, id) in materials"
+      :key="id"
+    >
+      <!-- Main content -->
+      <div 
+        class="max-w-64 px-4 border-l-4 border-buttonColor box-content text-black  relative flex-col transition-all duration-1000"
+        :class="{ 'hover:scale-95 duration-500': clickedElement !== id, 'transition-all duration-1000 ease-in-out': clickedElement === id}"
+        @click="toggleDescription(id)"
+      >
+        <img class="rounded  max-w-40 sm:max-w-48 " src="C:\Users\jojoe\Documents\USMB\Project\S4\logo\RentaVan\bleu\logo.png" alt="test" />
+
+        <div v-if="clickedElement != id" class="font-medium text-textColor1 decoration-buttonColor hover:text-hoverColor cursor-pointer ">
+          {{ material.name }}
+        </div>
+        <div v-if="clickedElement != id" class="text-textColor2">
+          {{ material.price }}€/j
+        </div>
       </div>
-      <div>          
-        {{ material.price }}€/j
+      
+      <!-- Description content -->
+      <div>
+        <div v-if="clickedElement === id" class="font-medium text-textColor1 decoration-buttonColor hover:text-hoverColor cursor-pointer ">
+            {{ material.name }}
+          </div>
+          <div v-if="clickedElement === id" class="text-textColor2">
+            {{ material.price }}€/j
+          </div>
+          
+        <div v-if="clickedElement === id" class="col-span-full description">
+          Description : 
+          {{ material.description }}
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-
 import axios from 'axios';
 
 export default {
   data() {
     return {
-      materials: []
+      materials: [],
+      clickedElement: null,
     };
   },
   mounted() {
     axios.get("http://localhost:3008/api/materials")
       .then(response => {
-        // Assuming your API returns an array of materials
         this.materials = response.data;
-        console.log(response.data);
       })
       .catch(error => {
         console.error('Error fetching materials:', error);
       });
+  },
+  methods: {
+    showDescription(id) {
+      this.clickedElement = id;
+    },
+    hideDescription(id) {
+      this.clickedElement = null;
+    },
+    toggleDescription(id) {
+      if (this.clickedElement === id) {
+        this.hideDescription(id);
+      } else {
+        this.showDescription(id);
+      }
+    },
   }
 };
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.vue'],
   theme: {
-    extend: {}
+    colors: {
+      'primary': '#672F6C',
+      'secondary': '#000000',
+      'tertiary': '#FFFFFF',
+      'buttonColor': '#672F6C',
+      'hoverColor': '#523c70',
+      'textColor1': '#000000', // Noir
+      'textColor2': '#666666', // Gris clair
+      'textColor3': '#CCCCCC', // Gris foncé
+    }
   },
   plugins: []
 }
