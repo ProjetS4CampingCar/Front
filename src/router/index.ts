402db2aca885dff2985a9cabe1e2fcc38d31ae7d
evilbutcool73 <bboyjosh73@gmail.com>
2024-03-12 08:58:20 +0100

    mise en place d'un vrai fichier AfficheMateriaux

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,17 @@
+
 import { createRouter, createWebHistory } from 'vue-router'
+
+import afficheMateriaux from "@/views/AfficheMateriaux.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path : '/info8/afficheMateriaux',
+      name :'afficheMateriaux',
+      componant : afficheMateriaux
+    }
+    
   ]
 })
 
