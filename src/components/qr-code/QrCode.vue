e6b7ee39dce992bee5be0ab9851463be68147cc2
Aekhy <102717242+Aekhy@users.noreply.github.com>
2024-03-23 13:31:15 +0100

    added fore/back ground color & default

--- src/components/qr-code/QrCode.vue
+++ src/components/qr-code/QrCode.vue
@@ -8,16 +8,22 @@ withDefaults(defineProps<{
   level?: Level
   // Generate QRcode as canvas or svg
   renderAs?: RenderAs
+  // Foreground color in hexa default is black
+  foreground?: string
+  // Background color in hexa default is white
+  background?: string
 }>(), {
   level: 'M',
   renderAs: 'svg',
+  foreground: '#000000',
+  background: '#ffffff'
 })
 
 </script>
 
 <template>
   <main>
-    <qrcode-vue :value="value" :level="level" :render-as="renderAs" />
+    <qrcode-vue :value="value" :level="level" :render-as="renderAs" :background="background" :foreground="foreground" />
   </main>
 </template>
 
