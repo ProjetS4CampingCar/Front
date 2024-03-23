a492eefab9e521cee96e8cdfda1d61fcdf9b18f6
Aekhy <102717242+Aekhy@users.noreply.github.com>
2024-03-23 13:13:18 +0100

    add component that generate a qrcode of value given in value

---
+++ src/components/qr-code/QrCode.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import QrcodeVue, { type Level, type RenderAs } from 'qrcode.vue'
+
+withDefaults(defineProps<{
+  // Value content of qrcode
+  value: string
+  // Error correction level (one of 'L', 'M', 'Q', 'H')
+  level?: Level
+  // Generate QRcode as canvas or svg
+  renderAs?: RenderAs
+}>(), {
+  level: 'M',
+  renderAs: 'svg',
+})
+
+</script>
+
+<template>
+  <main>
+    <qrcode-vue :value="value" :level="level" :render-as="renderAs" />
+  </main>
+</template>
+
+<style></style>
